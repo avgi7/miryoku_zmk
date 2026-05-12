9c2c2de7bf266717a6cd20182bc177a6c079e4ee
avgi7 <96153705+avgi7@users.noreply.github.com>
2026-05-13 01:40:30 +0300

    Update custom_config.h sym layer, macros, autoshift

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,3 +12,80 @@ U_UND,             U_CUT,             U_CPY,             U_PST,             U_RD
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+/ {
+    behaviors {
+        // Για κανονικά πλήκτρα (kp)
+        as: auto_shift {
+            compatible = "zmk,behavior-hold-tap";
+            #binding-cells = <2>;
+            tapping-term-ms = <180>;
+            quick-tap-ms = <0>;
+            flavor = "tap-preferred";
+            bindings = <&kp>, <&kp>;
+        };
+
+        // Για Macros (χρησιμοποιώντας &none που στην v0.3 λειτουργεί ως μπαλαντέρ)
+        mas: macro_auto_shift {
+            compatible = "zmk,behavior-hold-tap";
+            #binding-cells = <2>;
+            tapping-term-ms = <180>;
+            quick-tap-ms = <0>;
+            flavor = "tap-preferred";
+            bindings = <&none>, <&none>; 
+        };
+
+        macros {
+            // Obsidian Highlight: <u>
+        m_hi1: m_hi1 {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            bindings = <&kp LT &kp U &kp GT>;
+        };
+                // Obsidian Highlight: </u>
+        m_hi2: m_hi2 {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            bindings = <&kp LT &SLASH &kp U &kp GT>;
+        };
+        // JavaScript Arrow Function: =>
+        m_arrow: m_arrow {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            wait-ms = <10>; // Small delay for stability in v0.3
+            bindings = <&kp EQUAL &kp GT>;
+        };
+
+        // JavaScript Strict Equality: ===
+        m_eq3: m_eq3 {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            bindings = <&kp EQUAL &kp EQUAL &kp EQUAL>;
+        };
+
+        // JS/Python Not Equal: !=
+        m_not_eq: m_not_eq {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            bindings = <&kp EXCL &kp EQUAL>;
+        };
+        m_py_def: m_py_def {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            bindings = <&kp D &kp E &kp F &kp SPACE &kp LPAR &kp RPAR &kp COLON &kp RET &kp TAB>;
+        };
+    };
+};
+
+
+
+#define MIRYOKU_LAYER_SYM \
+&m_not_eq,        &m_arrow,            &mas m_hi2 m_hi1,  &m_py_def,     &as PRCNT TILDE,      &as PIPE BSLH,     &as MINUS UNDER,    &as CARET AMPS,   &as EXCL QUESTION,   &as SQT DQT,     \
+&m_eq3,           &kp LALT,            &kp LCTRL,         &none,         &none,                &as PLUS EQUAL,    &as RPAR LPAR,      &as RBKT LBKT,    &as GT LT,           &as RBRC LBRC,   \
+&none,            &none,               &none,             &none,         &none,                &as AT DLLR,       &as ASTRK HASH,     &as COMMA SEMI,   &as DOT COLON,       &as GRAVE SLASH, \
+U_NP,             U_NP,                &none,             &none,         &none,                &kp BSPC,          &kp SPACE,          &kp DEL,          U_NP,                U_NP
+
