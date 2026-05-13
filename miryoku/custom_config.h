70a137ab3b3bd3c81b055b72a476e2fbd767cd67
avgi7 <96153705+avgi7@users.noreply.github.com>
2026-05-13 23:35:55 +0300

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -29,10 +29,9 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 };
 
 
-
 #define MIRYOKU_LAYER_SYM \
 &m_arrow,         &m_not_eq,           &m_hi12,           &m_hi1,        &as PRCNT TILDE,      &as PIPE BSLH,     &as MINUS UNDER,    &as CARET AMPS,   &as QUESTION EXCL,   &as SQT DQT,     \
-&none,            &kp LALT,            &kp LCTRL,         &none,         &mas m_hi2 m_hi1,     &as PLUS EQUAL,    &as RPAR LPAR,      &as RBKT LBKT,    &as GT LT,           &as RBRC LBRC,   \
+&none,            &kp LALT,            &kp LCTRL,         &none,         &none,                &as PLUS EQUAL,    &as RPAR LPAR,      &as RBKT LBKT,    &as GT LT,           &as RBRC LBRC,   \
 &none,            &none,               &none,             &none,         &none,                &as AT DLLR,       &as ASTRK HASH,     &as COMMA SEMI,   &as DOT COLON,       &as GRAVE SLASH, \
 U_NP,             U_NP,                &none,             &none,         &none,                &kp BSPC,          &kp SPACE,          &kp DEL,          U_NP,                U_NP
 
